d0c94f5733cca30372cff1aa192dc4b9decb9065
12halima12 <halimaard1949@gmail.com>
2023-10-23 11:05:48 -0700

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,7 +11,7 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-unsigned int i;
+lnsigned int i;
 listint_t *new;
 listint_t *temp = *head;
 
@@ -23,7 +23,7 @@ new->n = n;
 new->next = NULL;
 
 	if (idx == 0)
- 	{
+	{
 	new->next = *head;
 	*head = new;
 	return (new);
